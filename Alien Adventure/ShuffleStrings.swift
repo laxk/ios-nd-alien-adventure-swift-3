673d337417599462b6d731b8f7025eb2bccf5f56
Lakshmikantha <lakshmikantha@akitra.com>
2016-11-16 18:41:23 -0800

    Operator Change

--- Alien Adventure/ShuffleStrings.swift
+++ Alien Adventure/ShuffleStrings.swift
@@ -11,7 +11,7 @@ extension Hero {
     func shuffleStrings(s1: String, s2: String, shuffle: String) -> Bool {
         
         // Check if s1, s2 and shuffle are all empty strings and if yes, return true
-        if (s1 == "" && s2 == "" && shuffle == ""){
+        if s1 + s2 + shuffle == "" {
             return true
         } else if (shuffle.characters.count != (s1.characters.count+s2.characters.count)){
             // If shuffle is not of equal length of s1 and s2 combined return false
