4fdf6cb5fa63bb01c9b68f6308aec4df9e0da842
Lakshmikantha <lakshmikantha@akitra.com>
2016-11-16 18:45:17 -0800

    Remove redundancy

--- Alien Adventure/LeastValuableItem.swift
+++ Alien Adventure/LeastValuableItem.swift
@@ -10,29 +10,27 @@ extension Hero {
     
     func leastValuableItem(inventory: [UDItem]) -> UDItem? {
         var leastValue: Int?  //Optional to store the least base value
-        var leastItem: UDItem?  //Optional to store the item with least base value
-        //If invetory has items the return the item with least bsae value
-        if inventory.count > 0 {
-                //Iterate over the inventory to find the item with least base value
-                for item in inventory {
-                    //print(inventory)
-                    let value = item.baseValue
-                    if let least = leastValue{ //to check the least value if there are more than 1 item
-                        if least>value{
-                            leastValue = value
-                            leastItem = item
-                        }
-                    } else{
-                        leastValue = value //to store the first value
-                        leastItem = item
-                    }
-                //print(item.baseValue)
-                //print(leastValue)
-                //print(leastItem)
+        var leastItem: UDItem? //Optional to store the item with least base value
+        //If invetory has items the return the item with least base value
+        
+        //Iterate over the inventory to find the item with least base value
+        for item in inventory {
+            //print(inventory)
+            let value = item.baseValue
+            if let least = leastValue{ //to check the least value if there are more than 1 item
+                if least>value{
+                    leastValue = value
+                    leastItem = item
+                }
+            } else{
+                leastValue = value //to store the first value
+                leastItem = item
             }
-            return leastItem
+            //print(item.baseValue)
+            //print(leastValue)
+            //print(leastItem)
         }
-        return nil
+        return leastItem
     }
 }
 
