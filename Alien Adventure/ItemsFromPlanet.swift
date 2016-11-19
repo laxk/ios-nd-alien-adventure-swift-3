b2e7cdc71dce37e12d1ab5b9d2b13c1044587e0e
Lakshmikantha <lakshmikantha@akitra.com>
2016-11-18 16:17:40 -0800

    Implement itemsFromPlanet method

--- Alien Adventure/ItemsFromPlanet.swift
+++ Alien Adventure/ItemsFromPlanet.swift
@@ -9,9 +9,15 @@
 extension Hero {
     
     func itemsFromPlanet(inventory: [UDItem], planet: String) -> [UDItem] {
-        return [UDItem]()
+        
+        var planetItems = [UDItem]()
+        for item in inventory {
+            if (item.historicalData["PlanetOfOrigin"]) as? String == planet {
+                planetItems.append(item)
+            }
+        }
+        return planetItems
     }
-    
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
